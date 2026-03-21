66620e430061ec476af3bff5ea690e821940c218
pranjulisaxena <pranjulisaxena3@gmail.com>
2026-03-21 23:40:50 +0530

    webhook

--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -7,18 +7,18 @@ import User from '../models/User.js'
 export const clerkWebhooks = async (req, res) =>{
 try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-    await whook.verify(req.body, {
-    "svix-id": req.headers["svix-id"],
-    "svix-timestamp": req.headers["svix-timestamp"],
-    "svix-signature": req.headers["svix-signature"]
-})
+    await whook.verify(JSON.stringify(req.body), {
+        "svix-id": req.headers["svix-id"],
+        "svix-timestamp": req.headers["svix-timestamp"],
+        "svix-signature": req.headers["svix-signature"]
+    })
     const {data, type} = req.body
 
     switch (type) {
         case 'user.created': {
             const userData = {
                 _id: data.id,
-                email: data.email_addresses?.[0]?.email_address || "",
+                email: data.email_addresses[0].email_address,
                 name: data.first_name + " " + data.last_name,
                 imageUrl: data.image_url, 
             }
