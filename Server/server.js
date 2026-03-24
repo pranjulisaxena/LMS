4ff51d2de8a34e4669aad49bf66e11b05c474db3
pranjulisaxena <pranjulisaxena3@gmail.com>
2026-03-24 20:05:03 +0530

    clerk webhook fixed

--- Server/server.js
+++ Server/server.js
@@ -4,12 +4,13 @@ import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 import { clerkMiddleware } from '@clerk/express'
-import User from './models/User.js'
+// import User from './models/User.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRoute.js'
 import userRouter from './routes/userRoutes.js'
 import { stripeWebhooks } from './controllers/stripeWebhooks.js'
+
 
 // Initialize Express
 const app = express()
@@ -28,28 +29,29 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 app.get('/', (req, res) =>{
     res.send('API is Working')
 })
-app.post('/api/user',express.json(), async (req, res) => {
-    // console.log(req.body);
-    const user = User.findById(req.body.clerkId);
+// app.post('/api/user',express.json(), async (req, res) => {
+//     // console.log(req.body);
+//     const user = User.findById(req.body.clerkId);
 
-    if(user){
-        return res.send("ok");
-    }
-    else{
-    const userData = {
-            _id: req.body.clerkId,
-            email: req.body.email,
-            name: req.body.name,
-            imageUrl: req.body.imageUrl, 
-        }
-    await User.create(userData)
-    }
-    res.send("Received");  
-})
+//     if(user){
+//         return res.send("ok");
+//     }
+//     else{
+//     const userData = {
+//             _id: req.body.clerkId,
+//             email: req.body.email,
+//             name: req.body.name,
+//             imageUrl: req.body.imageUrl, 
+//         }
+//     await User.create(userData)
+//     }
+//     res.send("Received");  
+// })
+app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator',express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
-// app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks )
+
 
 // Port
 const PORT = process.env.PORT || 5000
