fd502d5b5edac174536129f2084e984f168c6ac4
pranjulisaxena <pranjulisaxena3@gmail.com>
2026-03-25 16:57:06 +0530

    clerk webhook

--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -7,7 +7,9 @@ import User from '../models/User.js'
 export const clerkWebhooks = async (req, res) =>{
 try {
     console.log("Clerk Webhook Hit")
+
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+    
     await whook.verify(JSON.stringify(req.body), {
         "svix-id": req.headers["svix-id"],
         "svix-timestamp": req.headers["svix-timestamp"],
--- Server/server.js
+++ Server/server.js
@@ -28,19 +28,7 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 app.get('/', (req, res) =>{
     res.send('API is Working')
 })
-app.post('/api/user',express.json(), async (req, res) => {
-    // console.log(req.body);
-
-    const userData = {
-            _id: req.body.clerkId,
-            email: req.body.email,
-            name: req.body.name,
-            imageUrl: req.body.imageUrl, 
-        }
-    await User.create(userData)
-
-    res.send("Received");  
-})
+app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator',express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
